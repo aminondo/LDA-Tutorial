2ad85ff39d7734ac85a17931ac18269027383e53
Antonio Minondo <aminondo@nd.edu>
2017-06-15 11:18:06 +0200

    added visualization and some changes

--- LDA.R
+++ LDA.R
@@ -19,28 +19,30 @@ head(Smarket)
 train.index = sample(c(T, F), nrow(Smarket), replace = TRUE, prob = c(0.8, 0.2))
 market_data = split(Smarket, train.index)
 names(market_data) = c("test", "train")
-
 #use MASS lda function using train data
-lda_fit = lda(Direction ~ .,data=data$train)
+lda_fit = lda(Direction ~ .,data=market_data$train)
 lda_fit
+plot(lda_fit)
 
 #predict using test data
-lda_predict = predict(lda_fit, newdata = data$test)
+lda_predict = predict(lda_fit, newdata = market_data$test)
+plot(lda_predict)
 #confusion matrix to establish how accurate our results are
-table(lda_predict$class,data$test$Direction)
+table(lda_predict$class,market_data$test$Direction)
 
-# DEFAULT--------------------------------------------------------------------------------------------------------------------
-#Now we are going to use Default data to predict whether someone will default on a loan like we did in class yesterday
-head(Default)
 
-train.index = sample(c(T,F), nrow(Default), replace=T, prob = c(.8,.2))
-def_data = split(Default, train.index) 
-names(def_data) = c("test", "train")
+#VISUALIZING--------------------------------------------------------------
+#working with iris data set
+lda_fit = lda(Species ~ ., data=iris)
+lda_predict = predict(lda_fit, iris)
+lda_predict
+table(lda_predict$class,iris$Species)
+library(ggplot2)
+data = data.frame(species=iris[,"Species"],
+                  lda = lda_predict$x)
+ggplot(data) + geom_point(aes(lda.LD1,lda.LD2, color=species, shape=species))
 
-lda_fit = lda(default ~ ., data = def_data$train)
-lda_fit
-lda_predict = predict(lda_fit, newdata=def_data$test)
-table(lda_predict$class,def_data$test$default)
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 # EXERCISES:
@@ -52,7 +54,7 @@ head(Titanicp)
 # 2. use a linear discriminant analysis to predict whether a person survived or not
 
 
-# 1. Use a linear discrimant analysis to predict the species of the iris data set
+# 1. Use a linear discrimant analysis to predict whehter a student will default on a loan or not
 
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -66,10 +68,21 @@ data = split(Titanicp, train.index)
 names(data) = c("test", "train")
 lda_fit = lda(survived ~ .,data=data$train)
 lda_fit
-
+plot(lda_fit)
 lda_predict = predict(lda_fit, newdata = data$test)
 table(lda_predict$class,data$test$survived)
-# 2.
-lda_fit = lda(Species ~ ., data=iris)
-lda_predict = predict(lda_fit, iris)
-table(lda_predict$class,iris$Species)
+
+
+# DEFAULT--------------------------------------------------------------------------------------------------------------------
+#Now we are going to use Default data to predict whether someone will default on a loan like we did in class yesterday
+head(Default)
+
+train.index = sample(c(T,F), nrow(Default), replace=T, prob = c(.8,.2))
+def_data = split(Default, train.index) 
+names(def_data) = c("test", "train")
+
+lda_fit = lda(default ~ ., data = def_data$train)
+lda_fit
+
+lda_predict = predict(lda_fit, newdata=def_data$test)
+table(lda_predict$class,def_data$test$default)
